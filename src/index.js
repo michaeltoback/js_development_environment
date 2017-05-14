069eb9ecda9b859cb9fd97f67f818a5eabbdb3f4
michaeltoback <mit1955@yahoo.com>
2017-05-14 09:15:40 -0700

    mocks working including switching between mock and production server, and delete function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import {getUsers} from './api/userApi';
+import './index.css';
 
+import {getUsers, deleteUser} from './api/userApi';
+
+// Populate table of users via API call.
 getUsers().then(result => {
 	let usersBody = "";
 	
@@ -12,6 +15,20 @@ getUsers().then(result => {
 			<td>${user.email}</td>
 			</tr>`
 	});
+	
 	global.document.getElementById('users').innerHTML = usersBody;
+	
+	const deleteLinks = global.document.getElementsByClassName('deleteUser');
+	
+	// Must use array from to create a real array from a DOM collection
+	// getElementsByClassname only returns an "array-like" object
+	Array.from(deleteLinks, link => {
+		link.onclick = function(event){
+			const element = event.target;
+			event.preventDefault();
+			deleteUser(element.attributes["data-id"].value);
+			const row = element.parentNode.parentNode;
+			row.parentNode.removeChild(row);
+		};
+	});
 });
-
